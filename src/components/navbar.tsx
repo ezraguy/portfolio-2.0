7be0ae452f838f76acdb5149017bb43e90d8e806
Guy Ezra <55058653+ezraguy@users.noreply.github.com>
2023-10-12 15:48:42 +0300

    Adding navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,14 @@
+import "../styles/navbar.scss";
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='logo'>
+        <b>
+          Gu<span>y</span>Ez<span>r</span>a
+        </b>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
