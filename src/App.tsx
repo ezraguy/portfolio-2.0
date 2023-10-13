e9f984bf5181a5122082132f9a956ea814f92fac
Guy Ezra <55058653+ezraguy@users.noreply.github.com>
2023-10-13 11:49:01 +0300

    Adding hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
-import Navbar from "./components/navbar";
 import About from "./sections/about";
 import Hero from "./sections/hero";
 import Work from "./sections/work";
 
 function App() {
+  const cursor = useRef<any>(null);
+  const innerCircle = useRef<any>(null);
+  const handleMouseMove: (e: any) => void = (e: any) => {
+    const { clientX, clientY } = e;
+    cursor.current.style.visibility = "visible";
+    cursor.current.style.transform = `translate3d(${clientX}px, ${clientY}px , 0`;
+  };
+  const handleMouseHold: (isMouseDown: boolean) => void = (
+    isMouseDown: boolean
+  ) => {
+    innerCircle.current.style.opacity = isMouseDown ? 1 : 0;
+  };
+  useEffect(() => {
+    window.addEventListener("mousemove", (e) => handleMouseMove(e));
+    window.addEventListener("mousedown", () => handleMouseHold(true));
+    window.addEventListener("mouseup", () => handleMouseHold(false));
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
     <>
       <div className='App'>
+        <div className='circle' ref={cursor}>
+          <div className='innerCircle' ref={innerCircle}></div>
+        </div>
         <div className='container'>
-          <Navbar />
           <Hero />
           <About />
           <Work />
--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -1,11 +1,53 @@
-import Navbar from "../components/navbar";
+import { Canvas } from "@react-three/fiber";
+import { Sphere, OrbitControls, MeshDistortMaterial } from "@react-three/drei";
 import "../styles/sections/hero.scss";
+import Navbar from "../components/navbar";
 
 const Hero = () => {
   return (
     <div className='hero section'>
       <Navbar />
-      hero
+      <div className='left'>
+        <div className='content'>
+          <h1 className='name'>Hey, I'm Guy Ezra</h1>
+          <span className='subTitle'>Front End Developer</span>
+          <p className='desc'>
+            My biggest passion is to make a website come to life.
+          </p>
+        </div>
+      </div>
+      <div className='right'>
+        <Canvas className='canvas'>
+          <OrbitControls enableZoom={false} />
+          <ambientLight intensity={3} />
+          <directionalLight position={[1, 2, 3]} />
+          <Sphere args={[1, 100, 200]} scale={1.5}>
+            <MeshDistortMaterial
+              color='#3d1c56'
+              attach='material'
+              distort={0.5}
+              speed={2}
+            />
+          </Sphere>
+        </Canvas>
+      </div>
+      <div className='arrow'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='24'
+          height='24'
+          viewBox='0 0 24 24'
+          fill='none'
+          stroke='#fff'
+          strokeWidth='2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          className='feather feather-arrow-down'
+        >
+          <line x1='12' y1='5' x2='12' y2='19' />
+          <polyline points='19 12 12 19 5 12' />
+        </svg>
+      </div>
     </div>
   );
 };
